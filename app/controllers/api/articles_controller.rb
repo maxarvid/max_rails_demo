20e231251e0ad198bab2f73a80d08c85f5715608
maxarvid <max.arvid.anderson@gmail.com>
2021-09-21 14:32:00 +0200

    adds factoryBot articles in spec

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,11 @@
 class Api::ArticlesController < ApplicationController
   def index
-    render json: { message: 'There are no articles' }
+    articles = Article.all
+    if articles.any?
+      render json: {articles: articles}
+    else 
+      render json: { message: 'There are no articles' }
+    end
   end
 end
 
--- spec/requests/api/articles/index_spec.rb
+++ spec/requests/api/articles/index_spec.rb
@@ -1,14 +1,29 @@
 RSpec.describe 'GET /api/articles', type: :request do
+
+
+
   subject { response }
 
-  before do
-    get '/api/articles'
-  end
+  describe 'when there are some articles in the database' do
+    let!(:articles) do
+      create_list(:article, 2)
+      create(:article, title: 'my custom title', lede: 'amazing lede')
+    end
+    before do
+      
+      get '/api/articles'
+    end
 
-  xdescribe 'when there are some articles in the database' do
+    it 'is expected to return a collection of articles' do
+      expect(response_json['articles'].size).to eq 3
+    end
   end
 
   describe 'when there are NO articles in the database' do
+    before do
+      get '/api/articles'
+    end
+  
     it { is_expected.to have_http_status 200 }
 
     it 'is expected to respond with a message' do
